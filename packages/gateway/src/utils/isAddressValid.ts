c3a84d480964b7f90ab9802d710e5e04d5c92f1b
Gonzalo D'elia <gonzalo@hemi.xyz>
2026-01-29 16:44:15 -0300

    Make sure isAddressValid returns boolean

--- packages/gateway/src/utils/isAddressValid.ts
+++ packages/gateway/src/utils/isAddressValid.ts
@@ -1,4 +1,4 @@
 import { type Address, isAddress, isAddressEqual, zeroAddress } from "viem";
 
 export const isAddressValid = (address: Address | undefined) =>
-  address && isAddress(address) && !isAddressEqual(address, zeroAddress);
+  !!address && isAddress(address) && !isAddressEqual(address, zeroAddress);
