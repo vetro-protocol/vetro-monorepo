2021b5de8b1ca330a3a391050042769030a97325
Gonzalo D'elia <gonzalo@hemi.xyz>
2026-02-02 15:41:35 -0300

    Add token logo component

---
+++ web/src/components/tokenLogo/index.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import type { Token } from "types";
+
+type Props = Pick<Token, "logoURI" | "symbol">;
+
+export const TokenLogo = function ({ logoURI, symbol }: Props) {
+  const [hasError, setHasError] = useState(false);
+
+  if (!logoURI || hasError) {
+    return (
+      <div className="flex size-5 items-center justify-center overflow-hidden rounded-full border border-solid border-white bg-neutral-50 text-[6px] font-semibold text-ellipsis whitespace-nowrap text-neutral-700">
+        {symbol}
+      </div>
+    );
+  }
+
+  return (
+    <img
+      alt={`${symbol} logo`}
+      className="size-5 rounded-full"
+      onError={() => setHasError(true)}
+      src={logoURI}
+    />
+  );
+};
