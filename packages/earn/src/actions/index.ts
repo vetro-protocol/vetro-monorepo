545c8eb2bffd652196d2dd9b6611b0a5ed83c943
arturdolzan <arturdolzan@gmail.com>
2026-02-03 08:08:56 -0300

    Remove standard ERC-4626 methods from earn package
    These methods are already available in viem-erc4626 and don't need
    to be duplicated. Removed: getAsset, convertToAssets, convertToShares,
    previewWithdraw, getTotalAssets, getTotalSupply, getBalanceOf.
    Also trimmed unused ABI entries to reduce bundle size.

--- packages/earn/src/actions/index.ts
+++ packages/earn/src/actions/index.ts
@@ -1,18 +1,11 @@
 // Export all public actions
-export * from "./public/convertToAssets.js";
-export * from "./public/convertToShares.js";
 export * from "./public/getActiveRequestIds.js";
-export * from "./public/getAsset.js";
-export * from "./public/getBalanceOf.js";
 export * from "./public/getClaimableRequests.js";
 export * from "./public/getCooldownDuration.js";
 export * from "./public/getCooldownEnabled.js";
 export * from "./public/getPendingRequests.js";
 export * from "./public/getRequestDetails.js";
-export * from "./public/getTotalAssets.js";
 export * from "./public/getTotalAssetsInCooldown.js";
-export * from "./public/getTotalSupply.js";
-export * from "./public/previewWithdraw.js";
 
 // Export all wallet actions
 export * from "./wallet/cancelWithdraw.js";
--- packages/earn/src/index.ts
+++ packages/earn/src/index.ts
@@ -1,20 +1,13 @@
 import type { Client, WalletClient } from "viem";
 
 import {
-  convertToAssets,
-  convertToShares,
   getActiveRequestIds,
-  getAsset,
-  getBalanceOf,
   getClaimableRequests,
   getCooldownDuration,
   getCooldownEnabled,
   getPendingRequests,
   getRequestDetails,
-  getTotalAssets,
   getTotalAssetsInCooldown,
-  getTotalSupply,
-  previewWithdraw,
 } from "./actions/public/index.js";
 import {
   type CancelWithdrawParams,
@@ -57,16 +50,8 @@ export type {
 
 // Export factory functions for .extend() pattern
 export const earnPublicActions = () => (client: Client) => ({
-  convertToAssets: (params: Parameters<typeof convertToAssets>[1]) =>
-    convertToAssets(client, params),
-  convertToShares: (params: Parameters<typeof convertToShares>[1]) =>
-    convertToShares(client, params),
   getActiveRequestIds: (params: Parameters<typeof getActiveRequestIds>[1]) =>
     getActiveRequestIds(client, params),
-  getAsset: (params: Parameters<typeof getAsset>[1]) =>
-    getAsset(client, params),
-  getBalanceOf: (params: Parameters<typeof getBalanceOf>[1]) =>
-    getBalanceOf(client, params),
   getClaimableRequests: (params: Parameters<typeof getClaimableRequests>[1]) =>
     getClaimableRequests(client, params),
   getCooldownDuration: (params: Parameters<typeof getCooldownDuration>[1]) =>
@@ -77,15 +62,9 @@ export const earnPublicActions = () => (client: Client) => ({
     getPendingRequests(client, params),
   getRequestDetails: (params: Parameters<typeof getRequestDetails>[1]) =>
     getRequestDetails(client, params),
-  getTotalAssets: (params: Parameters<typeof getTotalAssets>[1]) =>
-    getTotalAssets(client, params),
   getTotalAssetsInCooldown: (
     params: Parameters<typeof getTotalAssetsInCooldown>[1],
   ) => getTotalAssetsInCooldown(client, params),
-  getTotalSupply: (params: Parameters<typeof getTotalSupply>[1]) =>
-    getTotalSupply(client, params),
-  previewWithdraw: (params: Parameters<typeof previewWithdraw>[1]) =>
-    previewWithdraw(client, params),
 });
 
 export const earnWalletActions = () => (client: WalletClient) => ({
