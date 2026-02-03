aa480fa43c2c84f798ba746cb5242d341765004f
Gonzalo D'elia <gonzalo@hemi.xyz>
2026-02-03 01:08:29 -0300

    Add MaxButton component

---
+++ web/src/components/base/maxButton.tsx
@@ -0,0 +1,19 @@
+type Props = {
+  disabled?: boolean;
+  onClick: () => void;
+};
+
+export const MaxButton = ({ disabled = false, onClick }: Props) => (
+  <button
+    className={`text-xsm font-semibold text-blue-500 ${
+      disabled
+        ? "cursor-not-allowed opacity-50"
+        : "cursor-pointer hover:text-blue-600"
+    }`}
+    disabled={disabled}
+    onClick={onClick}
+    type="button"
+  >
+    MAX
+  </button>
+);
