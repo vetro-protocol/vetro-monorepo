a2286d5619e94886273357e828fb732cc765e30f
arturdolzan <arturdolzan@gmail.com>
2026-02-04 08:07:24 -0300

    Minor improvements

--- web/src/pages/earn/icons/boltIcon.tsx
+++ web/src/pages/earn/icons/boltIcon.tsx
@@ -1,10 +1,10 @@
 export const BoltIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none">
+  <svg fill="none" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
     <path
+      clipRule="evenodd"
+      d="M9.58 1.077a.75.75 0 0 1 .405.82L9.165 6h4.085a.75.75 0 0 1 .567 1.241l-6.5 7.5a.75.75 0 0 1-1.302-.638L6.835 10H2.75a.75.75 0 0 1-.567-1.24l6.5-7.5a.75.75 0 0 1 .897-.183Z"
       fill="#416BFF"
       fillRule="evenodd"
-      d="M9.58 1.077a.75.75 0 0 1 .405.82L9.165 6h4.085a.75.75 0 0 1 .567 1.241l-6.5 7.5a.75.75 0 0 1-1.302-.638L6.835 10H2.75a.75.75 0 0 1-.567-1.24l6.5-7.5a.75.75 0 0 1 .897-.183Z"
-      clipRule="evenodd"
     />
   </svg>
 );
--- web/src/pages/earn/statCard/index.tsx
+++ web/src/pages/earn/statCard/index.tsx
@@ -10,7 +10,7 @@ export const StatCard = ({ icon, label, value }: Props) => (
   <div className="flex flex-col gap-3 border-t border-blue-500 px-3 py-6">
     <div className="flex items-center justify-between">
       <span className="text-xsm font-medium text-gray-900">{label}</span>
-      <div className="size-4 text-blue-500">{icon}</div>
+      <div className="size-4">{icon}</div>
     </div>
     <h3 className="text-2xl font-semibold text-gray-900">{value}</h3>
   </div>
