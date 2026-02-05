2298745a69601ce372099d49760dbe65bd4b3fd9
Gonzalo D'elia <gonzalo@hemi.xyz>
2026-02-05 15:37:26 -0300

    Add submit button for swap

---
+++ web/src/components/swapForm/submitButton.tsx
@@ -0,0 +1,92 @@
+import { useAllowance } from "@hemilabs/react-hooks/useAllowance";
+import { useConnectModal } from "@rainbow-me/rainbowkit";
+import { getGatewayAddress } from "@vetro/gateway";
+import { Button } from "components/base/button";
+import { Spinner } from "components/base/spinner";
+import { useMainnet } from "hooks/useMainnet";
+import type { ReactNode } from "react";
+import { useTranslation } from "react-i18next";
+import type { Token } from "types";
+import { useAccount } from "wagmi";
+
+import type { InputError } from "./types";
+
+const Container = ({ children }: { children: ReactNode }) => (
+  <div className="mt-2 flex w-full flex-col border-t border-gray-200 px-2 py-3">
+    {children}
+  </div>
+);
+
+type Props = {
+  actionText: string;
+  inputError: InputError;
+  token: Token;
+};
+
+export function SubmitButton({ actionText, inputError, token }: Props) {
+  const { address } = useAccount();
+  const { openConnectModal } = useConnectModal();
+  const ethereumChain = useMainnet();
+
+  const { data: allowance, isError: isAllowanceError } = useAllowance({
+    owner: address,
+    spender: getGatewayAddress(ethereumChain.id),
+    token,
+  });
+
+  const { t } = useTranslation();
+
+  if (!address) {
+    return (
+      <Container>
+        <Button
+          onClick={() => openConnectModal?.()}
+          size="xLarge"
+          type="button"
+        >
+          {t("pages.swap.form.connect-wallet")}
+        </Button>
+      </Container>
+    );
+  }
+
+  // show spinner for loading allowance
+  if (allowance === undefined && !isAllowanceError) {
+    return (
+      <Container>
+        <Button disabled size="xLarge" type="button">
+          <Spinner />
+        </Button>
+      </Container>
+    );
+  }
+
+  // show error if it failed to load allowance
+  if (isAllowanceError) {
+    return (
+      <Container>
+        <Button disabled size="xLarge" type="button">
+          {t("pages.swap.form.allowance-error")}
+        </Button>
+      </Container>
+    );
+  }
+
+  if (!inputError) {
+    return (
+      <Container>
+        <Button size="xLarge" type="submit">
+          {actionText}
+        </Button>
+      </Container>
+    );
+  }
+
+  return (
+    <Container>
+      <Button disabled size="xLarge" type="button">
+        {t(`pages.swap.form.${inputError}`)}
+      </Button>
+    </Container>
+  );
+}
