57e8f81c04207edd60240e22625f1ac29c670bfb
arturdolzan <arturdolzan@gmail.com>
2026-02-10 17:38:02 -0300

    Add polyfills for fetch-plus-plus

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,8 +2,17 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), cloudflare(), tailwindcss(), tsconfigPaths()],
+  plugins: [
+    react(),
+    cloudflare(),
+    nodePolyfills({
+      include: ["http", "https"],
+    }),
+    tailwindcss(),
+    tsconfigPaths(),
+  ],
 });
