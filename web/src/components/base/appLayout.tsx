16d3fa7c8b6dbcc685e0321da19b4a3976034d6d
Gonzalo D'elia <gonzalo@hemi.xyz>
2026-02-22 23:48:20 -0300

    Add background image at the bottom of the page

--- web/src/components/base/appLayout.tsx
+++ web/src/components/base/appLayout.tsx
@@ -3,7 +3,15 @@ import type { ReactNode } from "react";
 import "react-loading-skeleton/dist/skeleton.css";
 
 export const AppLayout = ({ children }: { children: ReactNode }) => (
-  <div className="flex h-full overflow-y-auto border-t border-gray-200">
-    <main className="mx-auto w-5xl border-x border-gray-200">{children}</main>
+  <div className="flex h-full flex-col overflow-y-auto border-t border-gray-200 *:mx-auto *:w-5xl">
+    <main className="border-x border-gray-200">{children}</main>
+    <div className="relative border-x border-gray-200 pt-18">
+      <img alt="" src="/pageBackground.svg" />
+      <img
+        alt=""
+        className="absolute right-0 bottom-0 left-0"
+        src="/squareDotsBackground.svg"
+      />
+    </div>
   </div>
 );
