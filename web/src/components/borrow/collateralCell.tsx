690c257734e30bd70244191cf73c87129367d521
Gonzalo D'elia <gonzalo@hemi.xyz>
2026-03-03 18:19:05 -0300

    Add token values in market table

--- web/src/components/borrow/collateralCell.tsx
+++ web/src/components/borrow/collateralCell.tsx
@@ -1,3 +1,4 @@
+import { RenderCryptoValue } from "components/base/cryptoValue";
 import { RenderFiatValue } from "components/base/fiatValue";
 import { useMarketCollateral } from "hooks/borrow/useMarketCollateral";
 import type { Token } from "types";
@@ -12,8 +13,24 @@ export function CollateralCell({ marketId, token }: Props) {
   const { data: value, status } = useMarketCollateral(marketId);
 
   return (
-    <span className="text-b-medium text-gray-900">
-      <RenderFiatValue queryStatus={status} token={token} value={value} />
-    </span>
+    <div className="items-end-end flex flex-col gap-y-0.5">
+      <span className="text-b-medium text-right text-gray-900">
+        <RenderFiatValue queryStatus={status} token={token} value={value} />
+      </span>
+      <span className="text-caption text-gray-500 *:w-full">
+        <RenderCryptoValue
+          amountContainer={({ children }) => (
+            <span className="block w-full text-right">{children}</span>
+          )}
+          container={({ children }) => (
+            <span className="flex w-full gap-x-0.5">{children}</span>
+          )}
+          showSymbol
+          status={status}
+          token={token}
+          value={value}
+        />
+      </span>
+    </div>
   );
 }
