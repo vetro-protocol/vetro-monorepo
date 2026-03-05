6ed56a35a16e1c3db0271d65597e092c2a7b1b4a
Gonzalo D'elia <gonzalo@hemi.xyz>
2026-03-05 00:04:34 -0300

    Split useMarketsData into useMarketData

--- web/src/hooks/borrow/useMarketsData.ts
+++ web/src/hooks/borrow/useMarketsData.ts
@@ -1,61 +1,18 @@
-import { type MarketId } from "@morpho-org/blue-sdk";
-import { Market } from "@morpho-org/blue-sdk-viem/lib/augment/Market";
-import { queryOptions, useQueries } from "@tanstack/react-query";
+import { useQueries } from "@tanstack/react-query";
 import { useMemo } from "react";
-import type { Token } from "types";
-import { unixNowTimestamp } from "utils/date";
-import type { Chain, Client, Hash } from "viem";
+import type { Hash } from "viem";
 
 import { useEthereumClient } from "../useEthereumClient";
 import { useMainnet } from "../useMainnet";
 import { tokenInfoOptions } from "../useTokenInfo";
 
-export const marketInfoQueryKey = ({
-  chainId,
-  marketId,
-}: {
-  chainId: Chain["id"];
-  marketId: Hash;
-}) => ["market-info", chainId, marketId];
+import {
+  type MarketData,
+  marketInfoOptions,
+  marketInfoQueryKey,
+} from "./useMarketData";
 
-const marketInfoOptions = ({
-  chainId,
-  client,
-  marketId,
-}: {
-  chainId: Chain["id"];
-  client: Client;
-  marketId: Hash;
-}) =>
-  queryOptions({
-    enabled: !!client,
-    async queryFn() {
-      const market = await Market.fetch(marketId as MarketId, client);
-      const accrued = market.accrueInterest(BigInt(unixNowTimestamp()));
-
-      return {
-        borrowApy: accrued.borrowApy,
-        collateralToken: market.params.collateralToken,
-        liquidity: market.liquidity,
-        lltv: market.params.lltv,
-        loanToken: market.params.loanToken,
-        totalBorrowAssets: market.totalBorrowAssets,
-        totalSupplyAssets: market.totalSupplyAssets,
-      };
-    },
-    queryKey: marketInfoQueryKey({ chainId, marketId }),
-  });
-
-export type MarketData = {
-  borrowApy: number;
-  collateralToken: Token;
-  liquidity: bigint;
-  lltv: bigint;
-  loanToken: Token;
-  marketId: Hash;
-  totalBorrowAssets: bigint;
-  totalSupplyAssets: bigint;
-};
+export { type MarketData, marketInfoQueryKey };
 
 export const useMarketsData = function (marketIds: Hash[]) {
   const ethereumChain = useMainnet();
