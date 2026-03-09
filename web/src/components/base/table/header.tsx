3b9d2e8f94fe00b25f26f057c05ecc6bd77f84ab
Gonzalo D'elia <gonzalo@hemi.xyz>
2026-03-09 16:36:39 -0300

    Add align functionality to table header

--- web/src/components/base/table/header.tsx
+++ web/src/components/base/table/header.tsx
@@ -1,5 +1,18 @@
-export const Header = ({ text }: { text: string }) => (
-  <span className="text-b-medium block py-3 text-left text-gray-500">
+const alignClasses = {
+  center: "text-center",
+  left: "text-left",
+};
+
+export const Header = ({
+  align = "left",
+  text,
+}: {
+  align?: "center" | "left";
+  text: string;
+}) => (
+  <span
+    className={`text-b-medium block py-3 ${alignClasses[align]} text-gray-500`}
+  >
     {text}
   </span>
 );
