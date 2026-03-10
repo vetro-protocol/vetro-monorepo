4d1528031e3976971d1cf8b61fc06362d0814f10
Gonzalo D'elia <gonzalo@hemi.xyz>
2026-03-10 11:16:29 -0300

    Update tokenValueCell to allow better alignment

--- web/src/components/borrow/tokenValueCell.tsx
+++ web/src/components/borrow/tokenValueCell.tsx
@@ -4,21 +4,35 @@ import { RenderFiatValue } from "components/base/fiatValue";
 import type { Token } from "types";
 
 type Props = {
+  align?: "left" | "right";
   status?: QueryStatus;
   token: Token;
   value: bigint | undefined;
 };
 
-export const TokenValueCell = ({ status = "success", token, value }: Props) => (
-  <div className="flex flex-col gap-y-0.5">
-    <span className="text-b-medium text-right text-gray-900">
+export const TokenValueCell = ({
+  align = "right",
+  status = "success",
+  token,
+  value,
+}: Props) => (
+  <div
+    className={`flex flex-col gap-y-0.5 ${align === "left" ? "items-start" : "items-end"}`}
+  >
+    <span
+      className={`text-b-medium ${align === "left" ? "text-left" : "text-right"} text-gray-900`}
+    >
       <span className="mr-1">$</span>
       <RenderFiatValue queryStatus={status} token={token} value={value} />
     </span>
     <span className="text-caption text-gray-500 *:w-full">
       <RenderCryptoValue
         amountContainer={({ children }) => (
-          <span className="block w-full text-right">{children}</span>
+          <span
+            className={`block w-full ${align === "left" ? "text-left" : "text-right"}`}
+          >
+            {children}
+          </span>
         )}
         container={({ children }) => (
           <span className="flex w-full gap-x-0.5">{children}</span>
