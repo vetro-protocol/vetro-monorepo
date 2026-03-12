85e53874b191a621324c1b8ff118419723dd8120
Gonzalo D'elia <gonzalo@hemi.xyz>
2026-03-12 14:46:44 -0300

    Update fees for borrow and supply

---
+++ web/src/hooks/borrow/useEstimateBorrowFees.ts
@@ -0,0 +1,58 @@
+import { useEstimateFees } from "@hemilabs/react-hooks/useEstimateFees";
+import { getChainAddresses } from "@morpho-org/blue-sdk";
+import { encodeBorrowAssets } from "@vetro/morpho-blue-market/actions";
+import { useMainnet } from "hooks/useMainnet";
+import type { createMorphoCollateralStateOverride } from "utils/morphoStateOverride";
+import type { Hash } from "viem";
+import { useAccount, useEstimateGas } from "wagmi";
+
+import { useMorphoMarket } from "./useMorphoMarket";
+
+type Params = {
+  borrowAmount: bigint;
+  canEstimate: boolean;
+  marketId: Hash;
+  stateOverride?: ReturnType<typeof createMorphoCollateralStateOverride>;
+};
+
+export function useEstimateBorrowFees({
+  borrowAmount,
+  canEstimate,
+  marketId,
+  stateOverride,
+}: Params) {
+  const ethereumChain = useMainnet();
+  const { address } = useAccount();
+  const morphoAddress = getChainAddresses(ethereumChain.id).morpho;
+
+  const { data: morphoMarket } = useMorphoMarket(marketId);
+
+  const canEstimateBorrow = canEstimate && morphoMarket !== undefined;
+
+  const { data: borrowGasUnits, isError: isBorrowGasUnitsError } =
+    useEstimateGas({
+      chainId: ethereumChain.id,
+      data: canEstimateBorrow
+        ? encodeBorrowAssets({
+            amount: borrowAmount,
+            marketParams: morphoMarket!.params,
+            onBehalf: address!,
+            receiver: address!,
+          })
+        : undefined,
+      query: {
+        enabled: canEstimateBorrow,
+        retry: false,
+      },
+      stateOverride,
+      to: morphoAddress,
+    });
+
+  const { fees, isError } = useEstimateFees({
+    chainId: ethereumChain.id,
+    gasUnits: borrowGasUnits,
+    isGasUnitsError: isBorrowGasUnitsError,
+  });
+
+  return { fees, isError };
+}
