eb59faf919d0cc4e29ba33504559c5e98a9b6322
Gonzalo D'elia <gonzalo@hemi.xyz>
2026-03-16 11:46:51 -0300

    Add redeemVaultToast to reduce eslint complexity

---
+++ web/src/components/swapForm/redeemVaultToasts.tsx
@@ -0,0 +1,48 @@
+import { Toast } from "components/base/toast";
+import { useTranslation } from "react-i18next";
+import type { Token } from "types";
+
+type Props = {
+  onClose: VoidFunction;
+  toastType: "cancel" | "redeem" | undefined;
+  toToken: Token;
+  vusd: Token;
+};
+
+export function RedeemVaultToasts({
+  onClose,
+  toastType,
+  toToken,
+  vusd,
+}: Props) {
+  const { t } = useTranslation();
+
+  if (toastType === "cancel") {
+    return (
+      <Toast
+        closable
+        description={t(
+          "pages.swap.redeem-vault.cancel-redeem-toast-description",
+          { symbol: vusd.symbol },
+        )}
+        onClose={onClose}
+        title={t("pages.swap.redeem-vault.cancel-redeem-toast-title")}
+      />
+    );
+  }
+
+  if (toastType === "redeem") {
+    return (
+      <Toast
+        closable
+        description={t("pages.swap.redeem-vault.swap-confirmed-description", {
+          symbol: toToken.symbol,
+        })}
+        onClose={onClose}
+        title={t("pages.swap.redeem-vault.redeem-confirmed")}
+      />
+    );
+  }
+
+  return null;
+}
