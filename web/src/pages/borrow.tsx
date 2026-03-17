1eb360c647d234945c8bb8e66354483f284d782d
Gonzalo D'elia <gonzalo@hemi.xyz>
2026-03-17 13:03:55 -0300

    Remove trailing comma

--- web/src/pages/borrow.tsx
+++ web/src/pages/borrow.tsx
@@ -9,7 +9,7 @@ import type { Hash } from "viem";
 const marketIds: Hash[] = [
   // hemiBTC / VUSD
   "0x55609be688a4d96e715bfe39969133bd4e7f83db4f77bb06216109189a11f2e5",
-  // WETH / VUSD,
+  // WETH / VUSD
   "0x7d1306d23f9f1e419697b8275001db9ea74b3c75190a7db8f5d81fed2fb94561",
 ];
 
