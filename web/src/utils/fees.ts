968f21108dae14d0394033b2293f614ae02f70f9
Gonzalo D'elia <gonzalo@hemi.xyz>
2026-03-17 17:41:56 -0300

    Add bps utility for fees

--- web/src/utils/fees.ts
+++ web/src/utils/fees.ts
@@ -1,3 +1,8 @@
+const BPS_DENOMINATOR = 10000n;
+
+export const applyBps = (amount: bigint, bps: bigint) =>
+  (amount * bps) / BPS_DENOMINATOR;
+
 export const sumFees = function (fees: (bigint | undefined)[]) {
   if (fees.every((fee): fee is bigint => fee !== undefined)) {
     return fees.reduce((sum, fee) => sum + fee, 0n);
