25a6d9bdea7ecf90daca3b054c9839b2f981d3ab
Gabriel Montes <gabriel@hemi.xyz>
2026-03-18 16:55:45 -0400

    Allow reduced animation

--- web/src/components/base/appLayout.tsx
+++ web/src/components/base/appLayout.tsx
@@ -7,7 +7,10 @@ export function AppLayout({ children }: { children: ReactNode }) {
   const { pathname } = useLocation();
   return (
     <div className="flex h-full flex-col overflow-y-auto *:mx-auto *:w-[calc(100%-2rem)] md:*:w-[calc(100%-6rem)] xl:*:w-5xl">
-      <main className="animate-page-in border-x border-gray-200" key={pathname}>
+      <main
+        className="motion-safe:animate-page-in border-x border-gray-200"
+        key={pathname}
+      >
         {children}
       </main>
       <div className="relative border-x border-gray-200 pt-18">
