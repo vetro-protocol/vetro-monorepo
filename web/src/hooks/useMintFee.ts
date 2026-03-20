11be9f87b0669defb27eb5ede7d8dc71bfaca9a7
Gonzalo D'elia <gonzalo@hemi.xyz>
2026-03-20 17:58:13 -0300

    Split useMintFee into hook and queryOptions

--- web/src/hooks/useMintFee.ts
+++ web/src/hooks/useMintFee.ts
@@ -1,33 +1,54 @@
-import { useQuery } from "@tanstack/react-query";
+import {
+  type UseQueryOptions,
+  queryOptions,
+  useQuery,
+} from "@tanstack/react-query";
 import { getGatewayAddress } from "@vetro/gateway";
 import { getMintFee } from "@vetro/gateway/actions";
-import type { Address, Chain } from "viem";
+import type { Address, Chain, Client } from "viem";
 
 import { useEthereumClient } from "./useEthereumClient";
 import { useMainnet } from "./useMainnet";
 
-const mintFeeQueryKey = ({
+type QueryOptions<TSelect = bigint> = Omit<
+  UseQueryOptions<bigint, Error, TSelect>,
+  "enabled" | "queryFn" | "queryKey"
+>;
+
+export const mintFeeOptions = <TSelect = bigint>({
   chainId,
+  client,
   gatewayAddress,
   token,
+  ...options
 }: {
   chainId: Chain["id"];
+  client: Client | undefined;
   gatewayAddress: Address;
   token: Address;
-}) => ["mint-fee", chainId, gatewayAddress, token];
+} & QueryOptions<TSelect>) =>
+  queryOptions({
+    ...options,
+    enabled: !!client,
+    queryFn: () => getMintFee(client!, { address: gatewayAddress, token }),
+    queryKey: ["mint-fee", chainId, gatewayAddress, token],
+  });
 
-export const useMintFee = function (token: Address) {
+export const useMintFee = function <TSelect = bigint>(
+  token: Address,
+  options?: QueryOptions<TSelect>,
+) {
   const ethereumChain = useMainnet();
   const client = useEthereumClient();
   const gatewayAddress = getGatewayAddress(ethereumChain.id);
 
-  return useQuery({
-    enabled: !!client,
-    queryFn: () => getMintFee(client!, { address: gatewayAddress, token }),
-    queryKey: mintFeeQueryKey({
+  return useQuery(
+    mintFeeOptions({
+      ...options,
       chainId: ethereumChain.id,
+      client,
       gatewayAddress,
       token,
     }),
-  });
+  );
 };
