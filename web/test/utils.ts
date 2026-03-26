71822485905aef5b332cfaee4d8b40e38fee8693
Gonzalo D'elia <gonzalo@hemi.xyz>
2026-03-26 09:36:11 -0300

    Add queryClient testing utility

---
+++ web/test/utils.ts
@@ -0,0 +1,8 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export const createTestQueryClient = () =>
+  new QueryClient({
+    defaultOptions: {
+      queries: { retry: false },
+    },
+  });
