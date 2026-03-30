4fe839c5151703cbe2dc24186f695bac4d4ef521
Gabriel Montes <gabriel@hemi.xyz>
2026-03-30 18:42:00 -0400

    Add chevron hover

--- web/src/components/tokenDropdown/index.tsx
+++ web/src/components/tokenDropdown/index.tsx
@@ -24,7 +24,6 @@ const renderItem = (token: Token) => (
 
 export const TokenDropdown = function ({ onChange, tokens, value }: Props) {
   const { t } = useTranslation();
-
   return (
     <Dropdown
       getItemKey={(token) => `${token.address}-${token.chainId}`}
@@ -35,11 +34,13 @@ export const TokenDropdown = function ({ onChange, tokens, value }: Props) {
       renderTrigger={(isOpen, triggerProps) => (
         <div
           {...triggerProps}
-          className={`button-secondary flex ${tokens.length > 1 ? "cursor-pointer" : ""} items-center gap-1.5 rounded-full py-1.5 pr-2 pl-1.5`}
+          className={`button-secondary flex ${tokens.length > 1 ? "cursor-pointer text-gray-500! hover:bg-gray-50! hover:text-neutral-900!" : ""} items-center gap-1.5 rounded-full py-1.5 pr-2 pl-1.5`}
         >
           <TokenDisplay logoURI={value.logoURI} symbol={value.symbol} />
           {tokens.length > 1 && (
-            <ChevronIcon direction={isOpen ? "up" : "down"} />
+            <div className="flex items-center">
+              <ChevronIcon direction={isOpen ? "up" : "down"} />
+            </div>
           )}
         </div>
       )}
