fbf08dafa39674ea0d907a46ea368f4e08b49b2d
Gonzalo D'elia <gonzalo@hemi.xyz>
2026-04-17 15:34:20 -0300

    Split EarnStats into per-stat components
    Extract EarnedAmountStat into its own component mirroring the
    StakedBalanceStat pattern, so each stat card owns its data fetching.
    EarnStats now just composes them, dropping the old single-vault
    hardcoding (share token, staked balance, token balance) that no longer
    fits the multi-vault model.

--- web/src/pages/earn/components/earnStats/index.tsx
+++ web/src/pages/earn/components/earnStats/index.tsx
@@ -1,60 +1,17 @@
-import { useTokenBalance } from "@hemilabs/react-hooks/useTokenBalance";
-import { stakingVaultAddresses } from "@vetro-protocol/earn";
-import { useMainnet } from "hooks/useMainnet";
-import { useShareToken } from "hooks/useShareToken";
-import { useStakedBalance } from "hooks/useStakedBalance";
-import { useVaultPeggedToken } from "hooks/useVaultPeggedToken";
 import { useTranslation } from "react-i18next";
-import { formatUnits } from "viem";
 
-import { BoltIcon } from "../../icons/boltIcon";
 import { SparklesIcon } from "../../icons/sparklesIcon";
-import { TrendingUpIcon } from "../../icons/trendingUpIcon";
+import { EarnedAmountStat } from "../earnedAmountStat";
+import { StakedBalanceStat } from "../stakedBalanceStat";
 import { StatCard } from "../statCard";
 
 export function EarnStats() {
   const { t } = useTranslation();
-  const chain = useMainnet();
-  // TODO using the only staking vault address to simplify this PR
-  // we will handle multiple addresses in the next PR
-  const stakingVaultAddress = stakingVaultAddresses[0];
-  const { data: shareToken } = useShareToken(stakingVaultAddress);
-  const { data: peggedToken } = useVaultPeggedToken(stakingVaultAddress);
-
-  const { data: stakedBalance, isLoading: isLoadingStakedBalance } =
-    useStakedBalance(stakingVaultAddress);
-
-  const { data: earnedAmount, isLoading: isLoadingEarnedAmount } =
-    useTokenBalance({ address: stakingVaultAddress, chainId: chain.id });
-
-  const formatStakedBalance = function () {
-    if (stakedBalance !== undefined && peggedToken) {
-      return `${formatUnits(stakedBalance, peggedToken.decimals)} ${peggedToken.symbol}`;
-    }
-    return "";
-  };
-
-  const formatEarnedAmount = function () {
-    if (earnedAmount !== undefined && shareToken) {
-      return `${formatUnits(earnedAmount, shareToken.decimals)} ${shareToken.symbol}`;
-    }
-    return "";
-  };
 
   return (
     <>
-      <StatCard
-        icon={<BoltIcon />}
-        isLoading={isLoadingStakedBalance}
-        label={t("pages.earn.stats.staked-balance")}
-        value={formatStakedBalance()}
-      />
-      <StatCard
-        icon={<TrendingUpIcon />}
-        isLoading={isLoadingEarnedAmount}
-        label={t("pages.earn.stats.earned-amount")}
-        value={formatEarnedAmount()}
-      />
+      <StakedBalanceStat />
+      <EarnedAmountStat />
       {/* TODO - Rewards should be implemented as soon we have the API available */}
       {/* See https://github.com/vetro-protocol/vetro-monorepo/issues/69 */}
       <StatCard
