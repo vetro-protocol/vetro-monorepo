395f9ff31488810587fcb78e6a9269257ed59930
Gonzalo D'elia <gonzalo@hemi.xyz>
2026-04-20 15:47:18 -0300

    Add seconds per day constant

--- web/src/pages/earn/components/exitTickets/dateCreatedCell.tsx
+++ web/src/pages/earn/components/exitTickets/dateCreatedCell.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
 import Skeleton from "react-loading-skeleton";
-import { formatDate } from "utils/date";
+import { formatDate, SECONDS_PER_DAY } from "utils/date";
 
 import { useCooldownDuration } from "../../hooks/useCooldownDuration";
 import type { ExitTicket } from "../../types";
@@ -25,7 +25,10 @@ export function DateCreatedCell({ ticket }: Props) {
 
   return (
     <span className="text-xsm font-normal text-gray-500">
-      {formatDate(Number(ticket.claimableAt) - data * 86400, i18n.language)}
+      {formatDate(
+        Number(ticket.claimableAt) - data * SECONDS_PER_DAY,
+        i18n.language,
+      )}
     </span>
   );
 }
