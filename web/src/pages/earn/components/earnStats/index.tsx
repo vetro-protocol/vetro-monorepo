cb4d20633842136975598521e4510c20d008955f
Gonzalo D'elia <gonzalo@hemi.xyz>
2026-06-22 11:06:02 -0300

    Remove rewards data from earn UI
    The rewards API isn't available yet, so the rewards UI only
    rendered placeholder "-" values and empty token stacks. Drop the
    useUserRewards hook, the potential-rewards items in the pool info
    bar, the rewards stat card, and the now-unused translation keys.

--- web/src/pages/earn/components/earnStats/index.tsx
+++ web/src/pages/earn/components/earnStats/index.tsx
@@ -1,24 +1,9 @@
-import { useTranslation } from "react-i18next";
-
-import { SparklesIcon } from "../../icons/sparklesIcon";
 import { EarnedAmountStat } from "../earnedAmountStat";
 import { StakedBalanceStat } from "../stakedBalanceStat";
-import { StatCard } from "../statCard";
 
-export function EarnStats() {
-  const { t } = useTranslation();
-
-  return (
-    <>
-      <StakedBalanceStat />
-      <EarnedAmountStat />
-      {/* TODO - Rewards should be implemented as soon we have the API available */}
-      {/* See https://github.com/vetro-protocol/vetro-monorepo/issues/69 */}
-      <StatCard
-        icon={<SparklesIcon />}
-        label={t("pages.earn.stats.rewards")}
-        value="-"
-      />
-    </>
-  );
-}
+export const EarnStats = () => (
+  <>
+    <StakedBalanceStat />
+    <EarnedAmountStat />
+  </>
+);
