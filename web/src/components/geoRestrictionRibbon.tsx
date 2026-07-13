d715c213e5c7dd82898519ff184e951c0ae78e87
Gabriel Montes <gabriel@hemi.xyz>
2026-07-13 18:06:06 -0300

    Add ribbon for restricted countries

---
+++ web/src/components/geoRestrictionRibbon.tsx
@@ -0,0 +1,19 @@
+import { useTranslation } from "react-i18next";
+
+import { CircleWarningIcon } from "./icons/circleWarningIcon";
+
+export function GeoRestrictionRibbon() {
+  const { t } = useTranslation();
+  return (
+    <div className="flex items-center justify-center gap-2 bg-gray-900 px-4 py-3">
+      <CircleWarningIcon />
+      <p className="text-b-medium text-white">
+        <span>{t("common.geo-restriction-title")}.</span>
+        <span className="text-white/64 max-md:hidden">
+          {" "}
+          {t("common.geo-restriction-description")}
+        </span>
+      </p>
+    </div>
+  );
+}
